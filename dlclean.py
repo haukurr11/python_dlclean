36a343930854133915ab3dc95547d5cb4855e5b8
pdarkness <knutur11@ru.is>
2013-08-05 23:53:26 +0000

    2nd

--- dlclean.py
+++ dlclean.py
@@ -1,5 +1,6 @@
 import re
 import os
+import shutil
 def dlclean(source,target,search):
     files = [ name for name in os.listdir(source)
     if os.path.isfile(os.path.join(source, name))
@@ -10,8 +11,19 @@ def dlclean(source,target,search):
             season = int(regEx.group(1))
             episode = int(regEx.group(2))
             print(season, episode)
-            
-        except:
+            targetPath = target+"\\"+search
+            fullPath = targetPath+"\\"+"Season "+str(season)
+            try:
+                os.mkdir(targetPath)
+            except FileExistsError:
+                pass
+            try:
+                os.mkdir(fullPath)
+            except FileExistsError:
+                pass
+            sour = source+"\\"+x
+            shutil.copyfile(sour, fullPath+"\\"+x)
+        except AttributeError:
             continue
             
     
