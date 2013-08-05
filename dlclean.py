8e2d36f1f5c64cbef138fcb5c316776ad245c49f
pdarkness <knutur11@ru.is>
2013-08-05 22:42:11 +0000

    sma stuff

--- dlclean.py
+++ dlclean.py
@@ -1,2 +1,17 @@
-def dlclean():
-    pass
+import re
+import os
+def dlclean(source,target,search):
+    files = [ name for name in os.listdir(source)
+    if os.path.isfile(os.path.join(source, name))
+    and search.lower() in name.replace('.', ' ').lower()]
+    for x in files:
+        try:
+            regEx = re.search("s(\d\d)e(\d\d)", x)
+            season = int(regEx.group(1))
+            episode = int(regEx.group(2))
+            print(season, episode)
+            
+        except:
+            continue
+            
+    
